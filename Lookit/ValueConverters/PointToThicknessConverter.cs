23052da1d42e539352eef75ca6da31b9649b08e8
SamMonballiu <45783609+SamMonballiu@users.noreply.github.com>
2022-11-13 18:03:09 +0100

    show area on polygon

---
+++ Lookit/ValueConverters/PointToThicknessConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Lookit.ValueConverters
+{
+    internal class PointToThicknessConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var center = (Point)values[0];
+            var textBlockWidth = (double)values[1];
+            var textBlockHeight = (double)values[2];
+
+            var left = center.X - (textBlockWidth / 2);
+            var top = center.Y - (textBlockHeight / 2);
+            return new Thickness(left, top, 0, 0);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Lookit/ViewModels/MeasurementViewModel.cs
+++ Lookit/ViewModels/MeasurementViewModel.cs
@@ -6,11 +6,12 @@ using System.Linq;
 
 namespace Lookit.ViewModels
 {
-    public class PolygonMeasurementViewModel: ObservableObject
+    public class PolygonMeasurementViewModel : ObservableObject
     {
         public PolygonalMeasurement Measurement { get; }
         public string Points => string.Join(",", Measurement.Points.Select(p => $"{p.X}, {p.Y}"));
         private Scale _scale;
+        public System.Windows.Point Center => GetCenter();
 
         public Scale Scale
         {
@@ -22,7 +23,24 @@ namespace Lookit.ViewModels
             }
         }
 
-        
+        private System.Windows.Point GetCenter()
+        {
+            var byX = Measurement.Points.OrderBy(pt => pt.X);
+            var byY = Measurement.Points.OrderBy(pt => pt.Y);
+
+            var leftMost = byX.First();
+            var rightMost = byX.Last();
+            var highest = byY.First();
+            var lowest = byY.Last();
+
+            var centerX = leftMost.X + (rightMost.X - leftMost.X) / 2;
+            var centerY = highest.Y + (lowest.Y - highest.Y) / 2;
+
+            return new System.Windows.Point(
+                    centerX, centerY
+                );
+        }
+
         public string Area => Math.Abs(Measurement.GetScaledArea(_scale) ?? 0).ToString("F");
 
         private PolygonMeasurementViewModel(PolygonalMeasurement measurement, Scale scale)
@@ -35,7 +53,7 @@ namespace Lookit.ViewModels
         public static PolygonMeasurementViewModel From(PolygonalMeasurement measurement, Scale scale) => new PolygonMeasurementViewModel(measurement, scale);
     }
 
-    public class LineMeasurementViewModel: ObservableObject
+    public class LineMeasurementViewModel : ObservableObject
     {
         public LineMeasurement Measurement { get; }
         private Scale _scale;
