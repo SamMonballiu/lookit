0a16ce5f73df941cc24a1dd235b61a1fe50b3dcf
SamMonballiu <45783609+SamMonballiu@users.noreply.github.com>
2023-04-09 15:10:33 +0200

    add try-catch to PointToThicknessConverter

--- Lookit/ValueConverters/PointToThicknessConverter.cs
+++ Lookit/ValueConverters/PointToThicknessConverter.cs
@@ -10,13 +10,19 @@ namespace Lookit.ValueConverters
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            var center = (Point)values[0];
-            var textBlockWidth = (double)values[1];
-            var textBlockHeight = (double)values[2];
+            try
+            {
+                var center = (Point)values[0];
+                var textBlockWidth = (double)values[1];
+                var textBlockHeight = (double)values[2];
 
-            var left = center.X - (textBlockWidth / 2);
-            var top = center.Y - (textBlockHeight / 2);
-            return new Thickness(left, top, 0, 0);
+                var left = center.X - (textBlockWidth / 2);
+                var top = center.Y - (textBlockHeight / 2);
+                return new Thickness(left, top, 0, 0);
+            } catch
+            {
+                return new Thickness(1);
+            }
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
