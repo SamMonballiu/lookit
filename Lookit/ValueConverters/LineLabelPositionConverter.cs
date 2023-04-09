6ffe6e27f9c7ecd73d43a66b97626f40eb9dca97
SamMonballiu <45783609+SamMonballiu@users.noreply.github.com>
2023-04-09 15:10:43 +0200

    add LineLabelPositionConverter

---
+++ Lookit/ValueConverters/LineLabelPositionConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Lookit.ValueConverters
+{
+
+    internal class LineLabelPositionConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var start = new Point((double)values[0], (double)values[1]);
+                var end = new Point((double)values[2], (double)values[3]);
+
+                var middle = new Point(System.Convert.ToInt32((start.X + end.X) / 2), System.Convert.ToInt32((start.Y + end.Y) / 2));
+
+                var labelWidth = (double)values[4];
+                var labelHeight = (double)values[5];
+
+                return new Thickness(middle.X - (labelWidth / 2), middle.Y - (labelHeight / 2), 0, 0);
+            }
+            catch
+            {
+                return new Thickness(0);
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
